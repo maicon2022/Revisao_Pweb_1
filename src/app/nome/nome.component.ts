ca349606e5789c2af3441de8c8a625620a3ebee1
maicon2022 <maicon.felipe@academico.ifpb.edu.br>
2024-07-16 23:05:40 -0300

    First Commit

---
+++ src/app/nome/nome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome',
+  templateUrl: './nome.component.html',
+  styleUrl: './nome.component.css'
+})
+export class NomeComponent {
+
+}
